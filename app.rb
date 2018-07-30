84aa2f8053a69b13e5cedc1a19075ee5a50642b4
Kevin Lee <kevin.lee21921@gmail.com>
2018-07-29 20:57:10 -0400

    fixed a lot of bugs. images appear. todo: change grid to bootstrap

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
-require_relative 'components/factory.rb'
+require_relative 'components/kitchen.rb'
+
+bakery0 = Bakery.new('Ruby Bakery')
+
+@cookies.each { |x| bakery0.add_to_products('cookies', x) }
+@cakes.each { |x| bakery0.add_to_products('cakes', x) }
+@muffins.each { |x| bakery0.add_to_products('muffins', x) }
 
 get'/' do
   erb :start
@@ -7,5 +13,6 @@ end
 
 get '/:product' do
   @target = params[:product]
+  @arr = bakery0.get_product(@target)
   erb :products
 end
---
+++ components/kitchen.rb
@@ -0,0 +1,28 @@
+require_relative 'class.rb'
+
+@cookies = [
+  Cookie.new('/media/cookie-0.jpg', 'Legend of Zelda cookies', '2.00'),
+  Cookie.new('/media/cookie-1.jpg', 'Peanut Butter / Chocolate Cookies', '1.50'),
+  Cookie.new('/media/cookie-2.jpeg', 'Assorted Cookies (Pack of 4)', '3.00'),
+  Cookie.new('/media/cookie-3.jpg', 'Sesame Street cookies', '2.00'),
+  Cookie.new('/media/cookie-4.jpg', 'Heart cookies', '2.00'),
+  Cookie.new('/media/cookie-5.jpg', 'Star Wars cookies', '2.00')
+]
+
+@cakes = [
+  Cake.new('/media/cake-0.png', 'Purple Floral Block Cake', '25.00'),
+  Cake.new('/media/cake-1.jpeg', 'White Floral Round Cake', '30.00'),
+  Cake.new('/media/cake-2.jpeg', 'Blue Floral Spiral Cake', '25.00'),
+  Cake.new('/media/cake-3.jpeg', 'Quartz Cake', '35.00'),
+  Cake.new('/media/cake-4.jpeg', 'White Floral Cake (extra floral)', '35.00'),
+  Cake.new('/media/cake-5.jpg', 'Orange Ombre Cake', '30.00')
+]
+
+@muffins = [
+  Muffin.new('/media/muffin-0.jpg', 'Chocolate Muffin', '2.50'),
+  Muffin.new('/media/muffin-1.jpg', 'Blueberry Muffin', '2.50'),
+  Muffin.new('/media/muffin-2.jpg', 'Stormtrooper Muffin', '3.50'),
+  Muffin.new('/media/muffin-3.jpg', 'Avengers Muffin', '3.50'),
+  Muffin.new('/media/muffin-4.jpg', 'Strawberry Oreo Muffin', '3.00'),
+  Muffin.new('/media/muffin-5.jpg', 'Avengers Muffin (alt style)', '3.50')
+]
