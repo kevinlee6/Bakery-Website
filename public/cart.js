a4d6628204c27f2a028d7eb88bb3e2964e5c3f33
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-08-04 16:54:38 -0400

    cart functionality half done

--- public/cart.js
+++ public/cart.js
@@ -1,14 +1,34 @@
-const add = document.getElementsByClassName('add');
+const cart = document.getElementById('cart');
+const cartList = {};
 
-const template =
-    `<div>
-        
-    </div>`;
+const template = (id, img, desc, price) => {
+    return `
+        <button onclick="cartRemove(${id})" class="cart-delete btn btn-danger">X</button>
+        <div class="img-container card-img-top">
+          <img class="cart-item" src=${img} alt=${desc} />
+        </div>
+        <div class="product-info card-body">
+          <h5 class="card-title">${desc}</h5>
+          <div class="price-container row">
+            <span class="card-text">$${price}</span>
+          </div>
+        </div>`;
+};
 
-for (let i of add) {
-    i.addEventListener('click', e => {
-        e.preventDefault();
+function cartRemove(id) {
+    const toRemove = document.getElementById(id);
+    cart.removeChild(toRemove);
+}
 
+function handleClick(event, id, img, desc, price) {
+    event.preventDefault();
 
-    })
+    let div = document.createElement('div');
+    div.id = id;
+    div.classList.add('card');
+    div.classList.add('fadein');
+    div.innerHTML = template(id, img, desc, price);
+    cart.appendChild(div);
+
+    cartList[id] = img;
 }
